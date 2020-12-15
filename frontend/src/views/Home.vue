8ef60001cbc3282a8b9fff50bb1d9a0a3ddde3cc
Aaron <eusahn@icloud.com>
2020-12-14 21:13:21 -0800

    Initial commit

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="text-center h-full w-full">Hello World {{ title }}</div>
+</template>
+
+<script>
+import { Vue, Component } from "vue-property-decorator";
+
+@Component({})
+export default class Home extends Vue {
+  title = "Hello World";
+}
+</script>
